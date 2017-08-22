c41337370b5b11ede449116b3edda0a0648889fb
Billy Jacoby <billyjacoby@nike.local>
2017-08-21 20:30:47 -0400

    added mongodb-uddate

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if (error) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB');
+
+  //findOneAndUpdate - takes (filter, update, options)
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('599b6f2e1a5496057e3f9612')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('599b5dc81a5496057e3f947d')
+  }, {
+    $set: {
+      name: 'Billy'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close()
+});
